d107a6edf27f144ba9e681b62ab26474c82b1e55
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-04 18:13:47 +0100

    changes

--- Statify.js
+++ Statify.js
@@ -7,21 +7,43 @@ while (e = r.exec(q)) {
 }
 const access_token = hashParams.access_token;
 
-// Utiliser le jeton d'accès pour récupérer les playlists de l'utilisateur
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.spotify.com/v1/me/player/currently-playing");
-xhr.setRequestHeader("Authorization", "Bearer " + access_token);
-xhr.onload = function() {
-    let html = "";
-    if (xhr.status === 200) {
-        const data = JSON.parse(xhr.responseText).items;
-        html+= "<div>"
-        html+= "<img alt='music_cover' src=" + data.item.images[0].url + ">";
-        html+= "<p>" + data.item.name + "</p>";
-        html+= "</div>"
-        document.getElementById("Cplaying").innerHTML = html;
-    } else {
-        console.log("Erreur de chargement des données");
-    }
-};
-xhr.send();
+// Utiliser le jeton d'accès pour récupérer les informations sur la musique en cours de lecture
+function getCurrentlyPlaying() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.spotify.com/v1/me/player/currently-playing");
+    xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+    xhr.onload = function() {
+        let html = "";
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            html += "<div class='currentlyPlaying'>";
+            html += "<img alt='album_cover' src='" + data.item.album.images[0].url+ "'>";
+            html += "<div class='currentlyPlayingInfo'>";
+            html += "<h2>" + data.item.name + "</h2>";
+            for (let i = 0; i < data.item.artists.length; i++) {
+                html += "<span>" + data.item.artists[i].name + "</span>";
+                if (i < data.item.artists.length - 1) {
+                    html += ", ";
+                }
+            }
+            html += "<br>";
+            html += "<br>";
+            html += "<span>" + Math.floor(data.progress_ms / 1000 / 60) + ":" + (Math.floor(data.progress_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.progress_ms / 1000 % 60) + " </span>";
+            html += "<progress id='progress' value='" + data.progress_ms + "' max='" + data.item.duration_ms + "'></progress>";
+            html += "<span> " + Math.floor(data.item.duration_ms / 1000 / 60) + ":" + (Math.floor(data.item.duration_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.item.duration_ms / 1000 % 60) + "</span>";
+            html += "</div>";
+            html += "</div>";
+            document.getElementById("Cplaying").innerHTML = html;
+            const progressBar = document.getElementById("progress");
+            progressBar.setAttribute("value", data.progress_ms);
+            progressBar.setAttribute("max", data.item.duration_ms);
+        } else {
+            console.log("Erreur de chargement des données");
+        }
+    };
+    xhr.send();
+}
+
+// Appeler la fonction de mise à jour de la barre de progression toutes les secondes
+getCurrentlyPlaying();
+setInterval(getCurrentlyPlaying, 1000);
