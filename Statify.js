a0b0e5f35d7ff9c711dfd3610e564f7834176336
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-04 19:14:56 +0100

    changes

--- Statify.js
+++ Statify.js
@@ -17,26 +17,58 @@ function getCurrentlyPlaying() {
         if (xhr.status === 200) {
             const data = JSON.parse(xhr.responseText);
             html += "<div class='currentlyPlaying'>";
-            html += "<img alt='album_cover' src='" + data.item.album.images[0].url+ "'>";
+            html += "<a href='" + data.item.uri + "' target='_blank'>";
+            html += "<img id='albumImage' alt='album_cover' src='" + data.item.album.images[0].url+ "'>";
+            html += "</a>";
             html += "<div class='currentlyPlayingInfo'>";
-            html += "<h2>" + data.item.name + "</h2>";
+            html += "<a class='name' href='" + data.item.uri + "' target='_blank'>";
+            html += "<h2 class='name'>" + data.item.name + "</h2>";
+            html += "</a><br>";
             for (let i = 0; i < data.item.artists.length; i++) {
                 html += "<span>" + data.item.artists[i].name + "</span>";
                 if (i < data.item.artists.length - 1) {
                     html += ", ";
                 }
             }
-            html += "<br>";
-            html += "<br>";
+            html += "<br><br><br>";
+            html += "<div id='progress_bar'>";
             html += "<span>" + Math.floor(data.progress_ms / 1000 / 60) + ":" + (Math.floor(data.progress_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.progress_ms / 1000 % 60) + " </span>";
             html += "<progress id='progress' value='" + data.progress_ms + "' max='" + data.item.duration_ms + "'></progress>";
             html += "<span> " + Math.floor(data.item.duration_ms / 1000 / 60) + ":" + (Math.floor(data.item.duration_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.item.duration_ms / 1000 % 60) + "</span>";
+            html +="</div>";
             html += "</div>";
             html += "</div>";
             document.getElementById("Cplaying").innerHTML = html;
             const progressBar = document.getElementById("progress");
             progressBar.setAttribute("value", data.progress_ms);
             progressBar.setAttribute("max", data.item.duration_ms);
+            const backgroundColor = data.item.album.images[0].dominant_color;
+            const albumImage = document.getElementById("albumImage");
+        } else {
+            console.log("Erreur de chargement des données");
+        }
+    };
+    xhr.send();
+}
+
+function getCurrentlyPlayingTime() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.spotify.com/v1/me/player/currently-playing");
+    xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+    xhr.onload = function() {
+        let html = "";
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            html += "<span>" + Math.floor(data.progress_ms / 1000 / 60) + ":" + (Math.floor(data.progress_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.progress_ms / 1000 % 60) + " </span>";
+            html += "<progress id='progress' value='" + data.progress_ms + "' max='" + data.item.duration_ms + "'></progress>";
+            html += "<span> " + Math.floor(data.item.duration_ms / 1000 / 60) + ":" + (Math.floor(data.item.duration_ms / 1000 % 60) < 10 ? "0" : "") + Math.floor(data.item.duration_ms / 1000 % 60) + "</span>";
+            document.getElementById("progress_bar").innerHTML = html;
+            const progressBar = document.getElementById("progress");
+            progressBar.setAttribute("value", data.progress_ms);
+            progressBar.setAttribute("max", data.item.duration_ms);
+            if (data.progress_ms <= 3000 ) {
+                getCurrentlyPlaying();
+            }
         } else {
             console.log("Erreur de chargement des données");
         }
@@ -46,4 +78,4 @@ function getCurrentlyPlaying() {
 
 // Appeler la fonction de mise à jour de la barre de progression toutes les secondes
 getCurrentlyPlaying();
-setInterval(getCurrentlyPlaying, 1000);
+setInterval(getCurrentlyPlayingTime, 1000);
