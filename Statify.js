98461bf92806296e2e43a757204945359a258843
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-04 13:11:47 +0100

    changes

--- Statify.js
+++ Statify.js
@@ -1,45 +1,26 @@
-// Initialiser l'authentification
-const authEndpoint = 'https://accounts.spotify.com/authorize';
-const clientId = '319eee2dc4eb4a9e8a957959d8e2d229';
-const redirectUri = 'http://localhost:3000/callback';
-const scopes = ['user-read-currently-playing'];
+// Récupérer le jeton d'accès à partir de l'URL
+const hashParams = {};
+let e, r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+while (e = r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+}
+const access_token = hashParams.access_token;
 
-// Rediriger l'utilisateur vers la page d'authentification Spotify
-window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
-
-// Fonction pour récupérer les paramètres de l'URL
-function getParamsFromUrl() {
-    const params = {};
-    const queryString = window.location.hash.substring(1);
-    const regex = /([^&=]+)=([^&]*)/g;
-    let m;
-    while ((m = regex.exec(queryString))) {
-        params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+// Utiliser le jeton d'accès pour récupérer les playlists de l'utilisateur
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.spotify.com/v1/me/playlists");
+xhr.setRequestHeader("Authorization", "Bearer " + access_token);
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        const playlists = JSON.parse(xhr.responseText).items;
+        let html = "";
+        for (let i = 0; i < playlists.length; i++) {
+            html += "<p>" + playlists[i].name + "</p>";
+        }
+        document.getElementById("playlists").innerHTML = html;
+    } else {
+        console.log("Erreur de chargement des playlists");
     }
-    return params;
-}
-
-// Fonction pour récupérer la musique actuellement écoutée par l'utilisateur
-function getCurrentlyPlayingTrack(accessToken) {
-    return fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-        headers: { 'Authorization': `Bearer ${accessToken}` }
-    })
-        .then(response => response.json())
-        .then(data => {
-            return {
-                name: data.item.name,
-                artist: data.item.artists[0].name,
-                album: data.item.album.name
-            };
-        });
-}
-
-// Récupérer les paramètres de l'URL après la redirection d'authentification
-const params = getParamsFromUrl();
-
-// Si l'utilisateur est authentifié, récupérer la musique actuellement écoutée
-if (params.access_token) {
-    getCurrentlyPlayingTrack(params.access_token)
-        .then(track => console.log(`Vous écoutez actuellement "${track.name}" de ${track.artist} (album: ${track.album})`))
-        .catch(error => console.error(error));
-}
+};
+xhr.send();
---
+++ index.js
@@ -0,0 +1,11 @@
+const client_id = '319eee2dc4eb4a9e8a957959d8e2d229';
+const redirect_uri = 'https://fmstatify.netlify.app/callback';
+const auth_endpoint = 'https://accounts.spotify.com/authorize';
+const scopes = 'user-read-private user-read-email playlist-read-private';
+
+document.getElementById('auth-button').addEventListener('click', function() {
+    window.location.href = auth_endpoint + '?response_type=token' +
+        '&client_id=' + encodeURIComponent(client_id) +
+        '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+        '&scope=' + encodeURIComponent(scopes);
+});
