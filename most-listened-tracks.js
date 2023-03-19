f6210aa794536fd6d3f53e58f716f96c0fcf40d6
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-03-19 15:03:01 +0100

    feat: top 10 tracks is now working

--- most-listened-tracks.js
+++ most-listened-tracks.js
@@ -1,43 +1,60 @@
-const xhr3 = new XMLHttpRequest();
-xhr3.open("GET", "https://api.spotify.com/v1/me/top/tracks?limit=10", true);
-xhr3.setRequestHeader('Authorization', 'Bearer ' + access_token);
-xhr3.setRequestHeader('Content-Type', 'application/json');
+document.addEventListener("DOMContentLoaded", function() {
+    let timeRange = document.querySelector("#time-range");
 
-let timeRange = document.querySelector("#time-range");
+    timeRange.addEventListener("change", function() {
+        if(this.value === "short_term") {
+            getTopTracks("short_term");
+        }
+        if(this.value === "medium_term") {
+            getTopTracks("medium_term");
+        }
+        if(this.value === "long_term") {
+            getTopTracks("long_term");
+        }
+    });
 
-timeRange.addEventListener("change", function () {
-    const selectedOption = this.options[timeRange.selectedIndex];
-    if (this.value === "short_term") {
-        console.log("short_term");
-    }
-    if (this.value === "medium_term") {
-        console.log("medium_term");
-    }
-    if (this.value === "long_term") {
-        console.log("long_term");
-    }
+    getTopTracks("short_term");
 });
 
-xhr3.onreadystatechange = function () {
-    const data = JSON.parse(xhr3.responseText);
+function getTopTracks(timeRange) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.spotify.com/v1/me/top/tracks?time_range=${timeRange}&limit=10`, true);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            displayTopTracks(data);
+        }
+    }
+    xhr.send();
+}
+
+function displayTopTracks(data) {
+    const topTracksContainer = document.getElementById("test");
+    topTracksContainer.innerHTML = "";
     for (let i = 0; i < data.items.length; i++) {
-        let html = "<a href='" + data.items[i].uri + "'><div class=\"menu-loader\">"
-        html += "<div class=\"menu-song\">"
-        html += "<p class='menu-name'>" + data.items[i].name + "</p>"
-        html += "<div class=\"menu-artist\">"
-        html += "<p class='menu-artist'>"
+        let html = "";
+        html += `<a href="${data.items[i].uri}">
+                    <div class="menu-loader">
+                        <div class="menu-song">
+                            <p class="menu-name">${data.items[i].name}</p>
+                            <div class="menu-artist">
+                                <p class="menu-artist">`;
         for (let k = 0; k < data.items[i].artists.length; k++) {
-            html += data.items[i].artists[k].name;
+            html += `${data.items[i].artists[k].name}`;
             if (k < data.items[i].artists.length - 1) {
-                html += ", ";
+                html += `, `;
             }
         }
-        html += "</div>"
-        html += "</div>"
-        html += "<img class=\"menu-albumcover\" src='" + data.items[i].album.images[1].url + "'>"
-        html += "<div class=\"menu-play\"></div>"
-        html += "</div></a>"
-        document.getElementById("mostListenedTracks").innerHTML += html;
+        html += `</p>
+                </div>
+            </div>
+            <img class="menu-albumcover" alt="album-cover" src="${data.items[i].album.images[1].url}">
+            <div class="menu-play"></div>
+        </div>
+    </a>`;
+        topTracksContainer.innerHTML += html;
     }
 }
-xhr3.send();
